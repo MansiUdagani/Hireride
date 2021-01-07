caad4ca80ae217399011cb5efea5540b85377a4d
MansiUdagani <77099044+MansiUdagani@users.noreply.github.com>
2021-01-07 15:47:29 +0530

    Update config.php

--- Carrental/includes/config.php
+++ Carrental/includes/config.php
@@ -1,8 +1,8 @@
 <?php
-	$host = "localhost";
-	$user = "root";
-	$pass = "root";
-	$db = "cars";
+	$host = "remotemysql.com";
+	$user = "rpzTA2weOd";
+	$pass = "No55Ru1eI4";
+	$db = "rpzTA2weOd";
 	
 	$conn = new mysqli($host, $user, $pass, $db);
 	if($conn->connect_error){
